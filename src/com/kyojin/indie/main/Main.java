826674115e5af6850ab7c927ca66f1d5652f48d3
kyojinindie <kyojinindie@gmail.com>
2020-11-17 22:14:31 -0600

    createDigest method implemented

--- src/com/kyojin/indie/main/Main.java
+++ src/com/kyojin/indie/main/Main.java
@@ -1,5 +1,6 @@
 package com.kyojin.indie.main;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -9,13 +10,16 @@ public class Main {
 	
 	private final static Logger LOGGER = Logger.getLogger("bitacora.subnivel.Control");
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchAlgorithmException {
 		ControllerImpl controller = new ControllerImpl();
 		String created = controller.generateUUIDCreate();
 		String expires = controller.generateUUIDExpires();
+		String canonicalTimestamp = controller.generatecanonicalTimestamp(created, expires);
+		String digest = controller.createDigest(canonicalTimestamp);
 		LOGGER.log(Level.INFO, "Create " + created);
 		LOGGER.log(Level.INFO, "Expires " + expires);
-		LOGGER.log(Level.INFO, "anonicalTimestamp " + controller.generatecanonicalTimestamp(created, expires));
+		LOGGER.log(Level.INFO, "anonicalTimestamp " + canonicalTimestamp);
+		LOGGER.log(Level.INFO, "digest " + digest);
 	}
 
 }
