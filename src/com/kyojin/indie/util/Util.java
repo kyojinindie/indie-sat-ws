b24d0ef69f8f1aba52d0193012ec798a9db3cdbe
Erick Lavielle <54757937+kyojinindie@users.noreply.github.com>
2020-12-11 16:33:06 -0600

    Update Util.java
    correction

--- src/com/kyojin/indie/util/Util.java
+++ src/com/kyojin/indie/util/Util.java
@@ -9,10 +9,10 @@ public class Util {
 	public static final String RFCRECEIVER = "";
 	public static final String RFCAPPLICANT = "";
 	public static final String TYPEREQUEST = "CFDI";
-	public final static String URLREQUESTDOWNLOAD = "https://cfdidescargamasivasolicitud.clouda.sat.gob.mx/SolicitaDescargaService.svc";
-    public final static String URLREQUESTDOWNLOADACTION = "http://DescargaMasivaTerceros.sat.gob.mx/ISolicitaDescargaService/SolicitaDescarga";
-    public final static String URLVERIFYREQUEST = "https://cfdidescargamasivasolicitud.clouda.sat.gob.mx/VerificaSolicitudDescargaService.svc";
-    public final static String URLVERIFYREQUESTACTION = "http://DescargaMasivaTerceros.sat.gob.mx/IVerificaSolicitudDescargaService/VerificaSolicitudDescarga";
-    public final static String URLDOWNLOADREQUEST = "https://cfdidescargamasiva.clouda.sat.gob.mx/DescargaMasivaService.svc";
-    public final static String URLDOWNLOADREQUESTACTION = "http://DescargaMasivaTerceros.sat.gob.mx/IDescargaMasivaTercerosService/Descargar";
+	public static final String URLREQUESTDOWNLOAD = "https://cfdidescargamasivasolicitud.clouda.sat.gob.mx/SolicitaDescargaService.svc";
+    	public static final String URLREQUESTDOWNLOADACTION = "http://DescargaMasivaTerceros.sat.gob.mx/ISolicitaDescargaService/SolicitaDescarga";
+    	public static final String URLVERIFYREQUEST = "https://cfdidescargamasivasolicitud.clouda.sat.gob.mx/VerificaSolicitudDescargaService.svc";
+    	public static final String URLVERIFYREQUESTACTION = "http://DescargaMasivaTerceros.sat.gob.mx/IVerificaSolicitudDescargaService/VerificaSolicitudDescarga";
+    	public static final String URLDOWNLOADREQUEST = "https://cfdidescargamasiva.clouda.sat.gob.mx/DescargaMasivaService.svc";
+    	public static final String URLDOWNLOADREQUESTACTION = "http://DescargaMasivaTerceros.sat.gob.mx/IDescargaMasivaTercerosService/Descargar";
 }
