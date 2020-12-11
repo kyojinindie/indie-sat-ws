c8b6bae324cbb4c333c45c18c8a79db402be26a6
kyojinindie <kyojinindie@gmail.com>
2020-12-11 12:02:59 -0600

    Adding verify objects

--- src/com/kyojin/indie/dto/Dto.java
+++ src/com/kyojin/indie/dto/Dto.java
@@ -1,22 +1,26 @@
 package com.kyojin.indie.dto;
 
-import com.kyojin.indie.client.Client;
 import com.kyojin.indie.client.ClientAuthentication;
 import com.kyojin.indie.client.ClientDownloadRequest;
+import com.kyojin.indie.client.ClientVerifyRequest;
 import com.kyojin.indie.model.Authentication;
 import com.kyojin.indie.model.RequestDownload;
-import com.kyojin.indie.util.Util;
+import com.kyojin.indie.model.VerifyRequest;
 
 public class Dto {
 	
 	Authentication authentication;
 	RequestDownload rqDownload;
+	VerifyRequest verifyRequest;
 	ClientAuthentication clientAuthentication;
 	ClientDownloadRequest clientDownloadRequest;
+	ClientVerifyRequest clientVerifyRequest;
 	
 	public Dto() {
 		this.authentication = new Authentication();
 		this.rqDownload = new RequestDownload();
+		this.verifyRequest = new VerifyRequest();
+		this.clientVerifyRequest = new ClientVerifyRequest();
 		this.clientAuthentication = new ClientAuthentication();
 		this.clientDownloadRequest = new ClientDownloadRequest();
 		}
@@ -36,6 +40,13 @@ public class Dto {
 	public ClientDownloadRequest getClientDownloadRequest() {
 		return clientDownloadRequest;
 	}
-	
+
+	public VerifyRequest getVerifyRequest() {
+		return verifyRequest;
+	}
+
+	public ClientVerifyRequest getClientVerifyRequest() {
+		return clientVerifyRequest;
+	}
 	
 }
